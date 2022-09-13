3ad7627d12a283603a29c2357ac0033da37ed3a0
Aarthiselvaraj <112369063+Aarthiselvaraj@users.noreply.github.com>
2022-09-13 10:14:52 +0530

    Add files via upload

--- Number_Pattern.java
+++ Number_Pattern.java
@@ -2,27 +2,22 @@ import java.util.Scanner;
 public class Number_Pattern {
 
 public static void main(String[] args) {
-
+int rows,num;
 Scanner sc=new Scanner(System.in);
-System.out.print("enter the no.of rows:");
-int n=sc.nextInt();
-
-for (int i = 1; i < n; i++) {
-
-
-for (int j = 1; j <= i; j++) {
-System.out.print("1");
+System.out.print("enter the number to be printed:");
+num=sc.nextInt();
+Scanner as=new Scanner(System.in);
+System.out.print("enter the max no.of time period:");
+rows=as.nextInt();
+for (int i=0; i<=rows-1;i++) {
+for (int j = 0; j <= i;j++) {
+System.out.print(num);
 }
-System.out.println();
-
+System.out.println("");
 }
-
-
-for (int i = n; i >= 1; i--) {
-
-
-for (int j = 1; j <= i; j++) {
-System.out.print("1");
+for (int i = rows-1; i >= 0; i--) {
+for (int j = 0; j <= i-1; j++) {
+System.out.print(num);
 }
 System.out.println();
 }
