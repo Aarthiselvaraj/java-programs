982a328d11318a48379dc283207dafd4985c6aa5
Aarthiselvaraj <112369063+Aarthiselvaraj@users.noreply.github.com>
2022-09-15 14:09:35 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class vowels{
+ public static void main(String[]args)
+  {
+   String str;
+   int count;
+   count = 0;
+   Scanner sa = new Scanner(System.in);
+   System.out.print("enter the string:")
+   str = sa.nextLine();
+   for (int i = 0; i < str.length(); i++){
+     if (str.charAt(i) =='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'||str.charAt(i)=='A'||str.charAt(i)=='E'||str.charAt(i)=='I'||str.charAt(i)=='O'||str.charAt(i)=='U')
+     count++;
+   }
+     System.out.println("Number of vowels :"+count);
+   
+  }
+}
